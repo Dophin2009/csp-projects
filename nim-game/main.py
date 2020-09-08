72a93c23f4031f70324fa234d768f2a02ca7cbd8
Eric Zhao <21zhaoe@protonmail.com>
2020-09-08 13:35:53 -0400

    Revert "nim-game: Pluggable interface"
    This reverts commit 8ee0c310a1971e14fa3272473a0aeae906f7b669.

--- nim-game/main.py
+++ nim-game/main.py
@@ -3,6 +3,18 @@ from __future__ import annotations
 import random
 
 
+def prompt_with(prompt: str) -> str:
+    return input(prompt + " ")
+
+
+def prompt_user_int(prompt: str) -> int:
+    num_str = prompt_with(prompt)
+    try:
+        return int(num_str)
+    except ValueError:
+        return prompt_user_int(prompt)
+
+
 class Pile:
     def __init__(self, sticks: int):
         self.sticks = sticks
@@ -16,33 +28,17 @@ class Pile:
         return Pile(sticks)
 
 
-class Interface:
-    def message(self, msg: str):
-        pass
-
-    def prompt_with(self, prompt: str) -> str:
-        pass
-
-    def prompt_user_int(self, prompt: str) -> int:
-        num_str = self.prompt_with(prompt)
-        try:
-            return int(num_str)
-        except ValueError:
-            return self.prompt_user_int(prompt)
-
-
 class Game:
-    def __init__(self, interface: Interface, piles: int, player_count: int):
-        self.interface = interface
+    def __init__(self, piles: int, player_count: int):
         self.piles = [Pile.random() for _ in range(0, piles)]
         self.player_count = player_count
         self.current_player = -1
 
     def loop(self):
         while not self.piles_clear():
-            self.message_status()
+            self.print_status()
             self.next_turn()
-        self.message("Game over: player {} lost".format(self.current_player))
+        print("Game over: player {} lost".format(self.current_player))
 
     def piles_clear(self) -> bool:
         for p in self.piles:
@@ -55,7 +51,7 @@ class Game:
         if self.current_player == self.player_count:
             self.current_player = 0
 
-        self.message("Player {}'s turn:".format(self.current_player))
+        print("Player {}'s turn:".format(self.current_player))
 
         pile = self.prompt_user_pile()
         take_count = self.prompt_user_take(pile)
@@ -65,7 +61,7 @@ class Game:
     def prompt_user_pile(self) -> Pile:
         prompt_msg = "Select pile ({} - {}):".format(
             0, self.player_count - 1)
-        choice = self.prompt_user_int(prompt_msg)
+        choice = prompt_user_int(prompt_msg)
 
         if choice >= len(self.piles) or choice < 0:
             return self.prompt_user_pile()
@@ -77,34 +73,19 @@ class Game:
         return pile
 
     def prompt_user_take(self, pile: Pile) -> int:
-        num = self.prompt_user_int(
+        num = prompt_user_int(
             "Select sticks (max {}):".format(pile.sticks))
         if num > pile.sticks or num < 1:
             return self.prompt_user_take(pile)
         return num
 
-    def prompt_user_int(self, prompt: str) -> int:
-        return self.interface.prompt_user_int(prompt)
-
-    def message(self, msg: str):
-        self.interface.message(msg)
-
-    def message_status(self):
+    def print_status(self):
         for i, p in enumerate(self.piles):
-            self.message("Pile {}: {}".format(i, p.sticks))
-
-
-class CommandLineInterface(Interface):
-    def message(self, msg: str):
-        print(msg)
-
-    def prompt_with(self, prompt: str) -> str:
-        return input(prompt + " ")
+            print("Pile {}: {}".format(i, p.sticks))
 
 
 if __name__ == "__main__":
-    interface = CommandLineInterface()
-    players = interface.prompt_user_int("Number of players?")
-    piles = interface.prompt_user_int("Number of piles?")
-    game = Game(interface, piles, players)
+    players = prompt_user_int("Number of players?")
+    piles = prompt_user_int("Number of piles?")
+    game = Game(piles, players)
     game.loop()
