a26d6ac9b4b438a632552403b6ec3435fb670c7a
Eric Zhao <21zhaoe@protonmail.com>
2020-09-13 15:38:35 -0400

    nim-game: Remove gui option

--- nim-game/main.py
+++ nim-game/main.py
@@ -5,21 +5,21 @@ import cli
 
 def flag_parser() -> ArgumentParser:
     parser = ArgumentParser(prog="nim", description="Play the game nim.")
-    parser.add_argument('-g', '--gui', action='store_true',
-                        help='use the QT GUI instead of cli')
+    #  parser.add_argument('-g', '--gui', action='store_true',
+    #  help='use the QT GUI instead of cli')
     return parser
 
 
 def main():
     parser = flag_parser()
-    args = parser.parse_args()
+    _ = parser.parse_args()
 
-    if args.gui:
-        exit(1)
-    else:
-        players = cli.prompt_user_int("Number of players?")
-        piles = cli.prompt_user_int("Number of piles?")
-        game = cli.Game(piles, players)
+    #  if args.gui:
+    #  exit(1)
+    #  else:
+    players = cli.prompt_user_int("Number of players?")
+    piles = cli.prompt_user_int("Number of piles?")
+    game = cli.Game(piles, players)
 
     game.loop()
 
